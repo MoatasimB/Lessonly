c399093daf413e25f9ab5e906d22817b68cd9009
Faris <fariskarim2000@gmail.com>
2024-12-29 18:51:02 -0500

    styling and font; add ability to change year

--- frontend/clientui/src/components/Nav.jsx
+++ frontend/clientui/src/components/Nav.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Nav = ({ onLoginClick, onSignupClick, teacherId, onLogoutClick }) => {
   return (
-    <div className="flex justify-between items-center bg-gray-700 w-full">
-      <div className="text-white px-10 font-bold">
-        <h1>APP NAME HERE</h1>
+    <div
+      className="flex justify-between items-center bg-gray-700 w-full"
+      style={{ fontFamily: "Comic Sans MS, Comic Sans, cursive" }}
+    >
+      <div className="text-white px-10 text-xl font-semi-bold">
+        <h1>Lessonly</h1>
       </div>
       <div className="p-4 px-10 justify-between flex gap-4 text-white font-bold text-right">
         {teacherId ? (
